b7fd7542619a78c4fe01b15d16226815a3479f40
SaiPriyankaG <103240861+SaiPriyankaG@users.noreply.github.com>
2022-04-28 21:45:21 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,16 @@
+REM   Script: Activity1
+REM   create table
+
+create table salesman;
+
+create table salesman;
+
+create table salesman(salesman_id int,salesman_name varchar2(20),salesman_city varchar2(20),commission int);
+
+create table salesman(salesman_id int,salesman_name varchar2(20),salesman_city varchar2(20),commission int);
+
+create table salesman(salesman_id int,salesman_name varchar2(20),salesman_city varchar2(20),commission int);
+
+describe salesman
+
+
